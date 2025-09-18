68d6b591755ff47aeb6d0b11c93ae0932c20f94d
trentonbates <77646214+trentonbates@users.noreply.github.com>
2025-09-18 19:10:03 +0000

    updated files

--- affine_cipher.py
+++ affine_cipher.py
@@ -1,17 +1,23 @@
+import cipherTools
+
 def affine_encrypt(plaintext_file, ciphertext_file, a, b):
-    if a < 1 or a > 25 or a % 2 == 0 or a == 1:
+    if a < 2 or a > 25 or a % 2 == 0:
         print('Unable to encrypt.')
         return
-    encrypted = ''
-    for char in alpha:
-        encrypted += alpha[(a * alpha.index(char.upper()) + b) % 26]
-    return encrypted
+    cipher_alpha = ''
+    for char in cipherTools.ALPHABET:
+        cipher_alpha += cipherTools.ALPHABET[(a * cipherTools.ALPHABET.index(char) + b) % 26]
+    cipherTools.encode(plaintext_file, ciphertext_file, cipher_alpha)
 
 def affine_decrypt(plaintext_file, ciphertext_file, a, b):
-    if a < 1 or a > 25 or a % 2 == 0 or a == 1:
+    if a < 2 or a > 25 or a % 2 == 0:
         print('Unable to decrypt.')
         return
-    encrypted = ''
-    for char in alpha:
-        encrypted += alpha[(pow(a, -1 , 26)*(alpha.index(char.upper())-b) % 26)]
-    return encrypted
+    cipher_alpha = ''
+    for char in cipherTools.ALPHABET:
+        cipher_alpha += cipherTools.ALPHABET[(pow(a, -1 , 26)*(cipherTools.ALPHABET.index(char)-b) % 26)]
+    cipherTools.decode(ciphertext_file, plaintext_file, cipher_alpha)
+
+affine_encrypt('liquid.txt', 'ciphertext.txt', 5, 3)
+
+affine_decrypt('ciphertext.txt', 'liquid.txt', 5, 3)
--- atbash_cipher.py
+++ atbash_cipher.py
@@ -1,15 +1,23 @@
 import cipherTools
 
 def atbash_encrypt(plaintext_file, ciphertext_file):
-    cipher_alpha = ''
-    for char in cipherTools.ALPHABET:
-        cipher_alpha += cipherTools.ALPHABET[-(cipherTools.ALPHABET.index(char)+1)]
+    '''
+    generates an alphabet used to encrypt a message with the atbash cipher
+
+    param plaintext_file: string of the location of file with message to be encoded
+    param ciphertext_file: string of the location of file that will have encrypted message written to it
+    '''
+    cipher_alpha = cipherTools.ALPHABET[::-1]
     cipherTools.encode(plaintext_file, ciphertext_file, cipher_alpha)
 
-def atbash_decrypt(plaintext_file, ciphertext_file):
-    cipher_alpha = ''
-    for char in cipherTools.ALPHABET[::-1]:
-        cipher_alpha += cipherTools.ALPHABET[-(cipherTools.ALPHABET.index(char)+1)]
+def atbash_decrypt(ciphertext_file, plaintext_file):
+    '''
+    generates an alphabet used to decrypt a message with the atbash cipher
+
+    param ciphertext_file: string of the location of file with encrypted message to be decoded
+    param plaintext_file: string of the location of file in which the decoded message will be written
+    '''
+    cipher_alpha = cipherTools.ALPHABET[::-1]
     cipherTools.decode(ciphertext_file, plaintext_file, cipher_alpha)
 
 #atbash_encrypt('liquid.txt','ciphertext.txt')
